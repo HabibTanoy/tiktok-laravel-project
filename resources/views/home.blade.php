91dbb2ec6fe535552a94f0a1c9a6c21540d17d82
HabibTanoy <hbtonoy42@gmail.com>
2021-10-23 00:07:45 +0600

    add images and css and convert in blading full project

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,243 @@
 @extends('master')
 @section('title','Download TikTok Video without watermark online for free')
 @section('content')
-@section
+<main>
+    <section>
+        <div class="splash-container pt-5" id="splash" hx-ext="include-vals">
+            <div class="splash" style="max-width: 1680px;">
+                <h1 class="splash-head hide-after-request" id="bigmessage">
+                    Download TikTok Video without watermark
+                </h1>
+                <form class="pure-form pure-g hide-after-request" id="_gcaptcha_pt" hx-post="/d3db68fd04b4">
+                    <div class="loader loader--style8 htmx-indicator pure-u-1" id="main_loader"></div>
+                    <div class="pure-u-1 pure-u-sm-18-24">
+                        <input id="main_page_text" title="https://m.tiktok.com/v/6606727368545406213.html" name="id" type="text" class="form-control input-lg" placeholder="Just insert a link" autofocus />
+                        <input id="locale" name="locale" type="hidden" value="en" />
+                    </div>
+                    <div class="pure-u-1 pure-u-sm-6-24">
+                        <button type="submit" id="submit" class="pure-button pure-button-primary u-fw">
+                            <span>Download</span>
+                        </button>
+                    </div>
+                </form>
+               
+
+                <div class="splash-subhead hide-after-request">
+                    Please, make sure your link looks like this:
+                    <pre>https://www.tiktok.com/@youneszarou/video/6942436555692805381<br>or<br>https://vt.tiktok.com/ZSJNV7cVn/</pre>
+                </div>
+                <!--link of tiktok-->
+                <div id="target">
+                    <div class="result u-shadow--black" id="mainpicture">
+                        <div class="result_overlay">
+                            <div class="row">
+                                <div class="col-md-2">
+                                <img class="u-round " src="./images/tiktok-img.png" alt="">
+                                </div>
+                                <div class="col-md-10">
+                                <p class="maintext mt-4">বিনোদন নিবা 😜😜 #🤪🤪 #blopper #dubai #comydy #pleaseunfrezzemyaccount</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <a href="" class="btn-block pure-button pure-button-primary text-center u-bl dl-button download_link without_watermark">
+                                        Without watermark
+                                    </a>
+                                </div>
+                            </div>
+                            <a href="" target="_blank" class="btn-block pure-button pure-button-primary text-center u-bl dl-button download_link without_watermark_direct">
+                                Without watermark [2]
+                            </a>
+                            <a href="" class="btn-block pure-button pure-button-primary text-center u-bl dl-button download_link music">
+                            Download MP3
+                            </a>
+                        </div>
+                    </div>          
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="content">
+            <div class="app_section text-center l-box" style="max-width: 1680px; margin: 0 auto;">
+                <h4 class="google-play" data-nosnippet>
+                    Our app is now available on Google Play
+                </h4>
+                <a class="app_banner" href="" target="_blank" rel="nofollow noopener">
+                    <img style="max-width: 240px;" src="./images/playstore.png" loading="lazy" alt="Download SSS Video Downloader for TikTok" />
+                </a>
+            </div>
+            <div class="highlights" style="max-width: 1680px; margin: 0 auto;">
+                <div class="pure-g">
+                    <div class="pure-u-1 pure-u-sm-1-24"></div>
+                    <div class="l-box pure-u-1 pure-u-sm-22-24">
+                        <div class="pure-g">
+                            <div class="pure-u-1 pure-u-sm-1-3 responsive">
+                                <div class="pure-g text-center u-m-bottom">
+                                    <div class="pure-u-5-24 pure-u-md-5-24 pure-u-sm-2-24"></div>
+                                    <div class="pure-u-14-24 pure-u-md-14-24 pure-u-sm-20-24">
+                                        <img src="./images/endless_.png" class="u-fw" />
+                                    </div>
+                                    <div class="pure-u-5-24 pure-u-md-5-24 pure-u-sm-2-24"></div>
+                                </div>
+                                <div class="content text-center">
+                                    <strong class="text-color--main u-bigger-text">Unlimited downloads</strong><br />
+                                    <br />
+                                    Download as many videos as you need, no limits or any other restrictions.
+                                </div>
+                            </div>
+
+                            <div class="pure-u-1 pure-u-sm-1-3 responsive">
+                                <div class="pure-g text-center u-m-bottom">
+                                    <div class="pure-u-5-24 pure-u-md-5-24 pure-u-sm-2-24"></div>
+                                    <div class="pure-u-14-24 pure-u-md-14-24 pure-u-sm-20-24">
+                                        <img src="./images/forbidden_.png" class="u-fw" />
+                                    </div>
+                                    <div class="pure-u-5-24 pure-u-md-5-24 pure-u-sm-2-24"></div>
+                                </div>
+                                <div class="content text-center">
+                                    <strong class="text-color--main u-bigger-text">No Watermark!</strong><br />
+                                    <br />
+                                    Download TT videos without a watermark, get any video without a TT logo.
+                                </div>
+                            </div>
+
+                            <div class="pure-u-1 pure-u-sm-1-3 responsive">
+                                <div class="pure-g text-center u-m-bottom">
+                                    <div class="pure-u-5-24 pure-u-md-5-24 pure-u-sm-2-24"></div>
+                                    <div class="pure-u-14-24 pure-u-md-14-24 pure-u-sm-20-24">
+                                        <img src="./images/photograph_.png" class="u-fw" />
+                                    </div>
+                                    <div class="pure-u-5-24 pure-u-md-5-24 pure-u-sm-2-24"></div>
+                                </div>
+                                <div class="content text-center">
+                                    <strong class="text-color--main u-bigger-text">MP4 and MP3 supported</strong><br />
+                                    <br />
+                                    Save videos in HD, .mp4 file format, or convert it to audio .mp3.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pure-u-1 pure-u-sm-1-24"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="splash-additional">
+            <div class="highlights" style="max-width: 1680px; margin: 0 auto;">
+                <div class="pure-g">
+                    <div class="pure-u-1 pure-u-sm-1-24"></div>
+                    <div class="l-box pure-u-1 pure-u-sm-22-24">
+                        <div class="text-center">
+                            <h2>Download Tik Tok video without watermark</h2>
+                            <p>
+                                <b>sssTikTok</b> is a free tool helping you download videos from TikTok without a watermark online. <b>sssTikTok</b> is one of the most popular online video downloaders that can help you save videos from
+                                TikTok with the highest quality in an MP4 file format with HD resolution. To find out how to use the TikTok downloader, follow the instructions below. It is easy. You can save videos in three simple steps.
+                            </p>
+                        </div>
+                        <hr />
+                        <h3 class="text-center">How to download TikTok videos without watermark?</h3>
+
+                        <ol>
+                            <li><b>Find a video</b> - play a video that you want to download to your mobile device, use the TikTok app;</li>
+                            <li><b>Copy a video link</b> - tap "Share" (the arrow button on top of a chosen video), and then tap "Copy link";</li>
+                            <li><b>Download</b> - go back to sssTikTok and paste the link in the text field on the page and tap "Download".</li>
+                        </ol>
+                    </div>
+                    <div class="pure-u-1 pure-u-sm-1-24"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="highlights" style="max-width: 1680px; margin: 0 auto;">
+            <div class="pure-g">
+                <div class="pure-u-1 pure-u-sm-1-24"></div>
+                <div class="l-box pure-u-1 pure-u-sm-22-24">
+                    <h3 class="text-center">Download any TikTok video on mobile phone (Android, iOS)</h3>
+                    <div class="pure-g u-m-bottom">
+                        <div class="pure-u-1 pure-u-md-12-24 pure-u-sm-6-24">
+                            <div class="pure-g text-center u-m-bottom">
+                                <div class="pure-u-5-24 pure-u-md-2-24 pure-u-sm-2-24"></div>
+                                <div class="pure-u-14-24 pure-u-md-20-24 pure-u-sm-20-24">
+                                    <img src="./images/video.png" class="u-fw" />
+                                </div>
+                                <div class="pure-u-5-24 pure-u-md-2-24 pure-u-sm-2-24"></div>
+                            </div>
+                        </div>
+                        <div class="pure-u-1 pure-u-md-12-24 pure-u-sm-18-24 u-smaller-text">
+                            <p>
+                                If you want to save a video on a mobile phone (Android or iOS), you need to launch the TikTok app and find a video you want to download. When you are on the video page, you will see the "Share" button on the
+                                right side of the screen, it looks like an arrow. Tap on it, and select "Copy link". After that, open sssTiktok webpage and paste the link into the text field on the top of the page. Then hit the "Download"
+                                button to get downloadable links. The last step is to click "Download without Watermark".
+                            </p>
+                            <p>If this method does not suit you, read the instructions below.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="pure-u-1 pure-u-sm-1-24"></div>
+            </div>
+
+            <div class="pure-g">
+                <div class="pure-u-1 pure-u-sm-1-24"></div>
+                <div class="l-box pure-u-1 pure-u-sm-22-24">
+                    <h3 class="text-center">Download TikTok video without watermark on PC</h3>
+                    <div class="pure-g u-m-bottom">
+                        <div class="pure-u-1 pure-u-md-12-24 pure-u-sm-18-24 u-smaller-text">
+                            <p>
+                                This method is universal and convenient because a video will be saved without watermark in original quality. It works perfectly on Windows, Mac OS, and Linux. PC users are not required to install any
+                                additional apps, and this is another plus of this method.
+                            </p>
+                            <p>In order to use the TikTok video downloader on PC, Mac, or laptop you will need to copy a video link, from the Tik Tok website.</p>
+                            <p>Next, go back to sssTikTok and paste the link in the text field on the main page. After that, you need to click on the "Download" button to get the links.</p>
+                        </div>
+                        <div class="pure-u-1 pure-u-md-12-24 pure-u-sm-6-24">
+                            <div class="pure-g text-center u-m-bottom">
+                                <div class="pure-u-5-24 pure-u-md-2-24 pure-u-sm-2-24"></div>
+                                <div class="pure-u-14-24 pure-u-md-20-24 pure-u-sm-20-24">
+                                    <img src="./images/download.png" class="u-fw" />
+                                </div>
+                                <div class="pure-u-5-24 pure-u-md-2-24 pure-u-sm-2-24"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="pure-u-1 pure-u-sm-1-24"></div>
+            </div>
+
+            <div class="pure-g">
+                <div class="pure-u-1 pure-u-sm-1-24"></div>
+                <div class="l-box pure-u-1 pure-u-sm-22-24">
+                    <h3 class="text-center">Download TikTok videos on iPhone or iPad (iOS)</h3>
+                    <div class="pure-g u-m-bottom">
+                        <div class="pure-u-1 pure-u-md-12-24 pure-u-sm-6-24">
+                            <div class="pure-g text-center u-m-bottom">
+                                <div class="pure-u-5-24 pure-u-md-2-24 pure-u-sm-2-24"></div>
+                                <div class="pure-u-14-24 pure-u-md-20-24 pure-u-sm-20-24">
+                                    <img src="./images/videomedia.png" class="u-fw" />
+                                </div>
+                                <div class="pure-u-5-24 pure-u-md-2-24 pure-u-sm-2-24"></div>
+                            </div>
+                        </div>
+                        <div class="pure-u-1 pure-u-md-12-24 pure-u-sm-18-24 u-smaller-text">
+                            <p>
+                                If you are an iPhone or iPad owner, you can also use a TikTok downloader. But you need to install the "Documents by Readle" app from the AppStore. As due to Apple security policy, iOS users starting with the
+                                12th version, can't save videos directly from the browser. Copy the link of any TikTok video via TikTok app, and launch Documents by Readle app.
+                            </p>
+                            <p>In the bottom right corner of the screen, you will see a web browser icon. Tap it.</p>
+                            <p>
+                                When the browser is open, go to ssstik.io and paste the link in the text field, and tap the "Download" button to see results. Choose the option you like and press the "Download" button again. The video will
+                                be saved to your device.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="pure-u-1 pure-u-sm-1-24"></div>
+            </div>
+        </div>
+    </section>
+</main>
+
+@endsection
